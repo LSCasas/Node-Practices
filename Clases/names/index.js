32a013522306e5d427bd14aeb05f1457550a36eb
Luis Casas <luisscasas45@gmail.com>
2024-05-15 19:46:50 -0600

    new programming of names

---
+++ Clases/names/index.js
@@ -0,0 +1,37 @@
+const prompt = require("prompt-sync")();
+let nombres = [];
+let continuar = true;
+while (continuar) {
+  const respuesta = prompt("Ingresa un nombre, finaliza la lista al escribir 'fin': ");
+  if (respuesta === "fin") continuar = false;
+  else nombres.push(respuesta);
+}
+
+console.log("El número de nombres ingresados fue:", nombres.length);
+console.log("Nombres ingresados:", nombres);
+
+// Encontrar el nombre repetido y cuántas veces se repite
+const contadorNombres = {};
+let nombreRepetido = null;
+let cantidadRepetida = 0;
+nombres.forEach(nombre => {
+  contadorNombres[nombre] = (contadorNombres[nombre] || 0) + 1;
+  if (contadorNombres[nombre] > cantidadRepetida) { 
+    nombreRepetido = nombre;
+    cantidadRepetida = contadorNombres[nombre];
+  }
+});
+console.log("Nombre más repetido:", nombreRepetido, ", veces repetido:", cantidadRepetida);
+
+// Encontrar el nombre más largo y el más corto
+let nombreMasLargo = nombres[0];
+let nombreMasCorto = nombres[0];
+nombres.forEach(nombre => {
+  if (nombre.length > nombreMasLargo.length) nombreMasLargo = nombre;
+  if (nombre.length < nombreMasCorto.length) nombreMasCorto = nombre;
+});
+console.log("Nombre más largo:", nombreMasLargo);
+console.log("Nombre más corto:", nombreMasCorto);
+
+
+    
